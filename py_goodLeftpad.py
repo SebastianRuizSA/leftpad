1fb262c832585c92ad0b60b7ba115e2622fdeb5b
SebastianRuizSA <sebastian90ruiz33@gmail.com>
2023-04-29 14:10:45 -0400

    Algoritmo optimizado con libreria

---
+++ py_goodLeftpad.py
@@ -0,0 +1,6 @@
+
+
+def leftpad(string, leng, char):
+    return (string.rjust(leng, char)) #leng es el tamannio del string resultante, char es el valor para rellenar(la funcion no acepta que sea de valor >1.
+
+
